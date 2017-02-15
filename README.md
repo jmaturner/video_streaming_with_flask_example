406245f975d4b6e34235f32e50babc9dbd077906
jmaturner <jmaturner@gmail.com>
2017-02-15 08:21:51 -0800

    adding password protection
    also added additional buttons on the flask page to control GPIO pins

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 ## Video Streaming with Flask Example
 
-### Website
-http://www.chioka.in
+### credits go to:
+http://www.chioka.in and Miguel Grinberg
 
 ### Description
-Modified to support streaming out with webcams, and not just raw JPEGs.
+Modified to support page authentication (Flask Basic Auth). Also instructions on making stream viewable remotely.
 
-### Credits
-Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
-http://blog.miguelgrinberg.com/post/video-streaming-with-flask
 
-### Usage
-1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
-2. Run "python main.py".
-3. Navigate the browser to the local webpage.
--- main.py
+++ main.py
@@ -1,26 +1,17 @@
+
 #!/usr/bin/env python
-#
-# Project: Video Streaming with Flask
-# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
-# Date: 2014/12/21
-# Website: http://www.chioka.in/
-# Description:
-# Modified to support streaming out with webcams, and not just raw JPEGs.
-# Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
-# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-#
-# Usage:
-# 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
-# 2. Run "python main.py".
-# 3. Navigate the browser to the local webpage.
-from flask import Flask, render_template, Response
+# File    : pystream.py ; a Flask app to live stream a usb camera in browser 
+# Author  : John Turner
+# Version : 0.3  March/2016
+#/>.
+
+
+from flask import Flask, render_template, Markup, Response, request, redirect, session
+from flask_basicauth import BasicAuth
 from camera import VideoCamera
+import smtplib
+import time
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def gen(camera):
     while True:
@@ -28,10 +19,34 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
+app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = 'username'
+app.config['BASIC_AUTH_PASSWORD'] = 'passwd'
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+@basic_auth.required
+def index():
+	return render_template('template.html', bodyText="Welcome to my webcam")
+
+
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+        return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+	
 
+	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+	
+	app.run(host='0.0.0.0', port=79)
+	#the port must be open on your router I used 79 here.
+	#use something obscure.. Have a strong password on the pi
+	#In your router settings enter local IP of pi with port 79 forwarded.
+	#Then visit www.yourdomain.com:79 or your_ip:79
+	
+	
