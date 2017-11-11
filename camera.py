f404c21c6f4a98b398a0b3bc8cb2b1b09ec06826
John <jmaturner@gmail.com>
2017-11-11 06:36:02 -0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,9 +2,7 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        #this requires openCV
         self.video = cv2.VideoCapture(0)
-
     
     def __del__(self):
         self.video.release()
